3d3e735dbfeadfa48f45c3e7e6277553076be4af
Mishel <mishelvettukattil@outlook.com>
2020-08-26 16:48:02 +0530

    Basic razor syntax

---
+++ BlazorApp/Client/Helpers/ConsoleHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Client.Helpers
+{
+    public class ConsoleHelper
+    {
+        public static void WriteToConsole(string text = "Hi form Blazor App")
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
