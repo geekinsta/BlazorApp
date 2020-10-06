135828d65e5576c83b2d353d8c836116ca8aa1e9
Mishel <mishelvettukattil@outlook.com>
2020-10-06 21:12:12 +0530

    Passing parameters

---
+++ BlazorApp/Shared/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+    }
+}
